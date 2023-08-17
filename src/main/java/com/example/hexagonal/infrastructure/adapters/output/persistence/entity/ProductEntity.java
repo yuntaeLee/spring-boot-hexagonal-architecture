43847f42e81eca3880f3c2c83193668cf4d023bd
yoon <leeyt1201@gmail.com>
2023-08-17 20:32:17 +0900

    feat: implemented product jpa entity

---
+++ src/main/java/com/example/hexagonal/infrastructure/adapters/output/persistence/entity/ProductEntity.java
@@ -0,0 +1,29 @@
+package com.example.hexagonal.infrastructure.adapters.output.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
