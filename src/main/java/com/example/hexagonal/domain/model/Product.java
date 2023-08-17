e233fd0b71cdd206562dcee3f75718c4cd2c9ebc
yoon <leeyt1201@gmail.com>
2023-08-17 20:29:30 +0900

    feat: implemented product domain model

---
+++ src/main/java/com/example/hexagonal/domain/model/Product.java
@@ -0,0 +1,21 @@
+package com.example.hexagonal.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
