a3fb63387bd36b6f402b2934f5224cffbab918d0
yoon <leeyt1201@gmail.com>
2023-08-19 20:35:04 +0900

    feat: implemented product creation event model

---
+++ src/main/java/com/example/hexagonal/domain/event/ProductCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.example.hexagonal.domain.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCreatedEvent {
+
+    private Long id;
+
+    private LocalDateTime createdAt;
+}
